4935fc332f3224e519eab3cb5e42b3994a5316f0
Salehisaac <salehisaac200281@gmail.com>
2023-05-31 13:12:44 +0330

    creating config

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+
+//session start
+session_start();
+
+//config
+define('BASE_PATH', __DIR__);
+define('CURRENT_DOMAIN', currentDomain() . '/project/');
+define('DISPLAY_ERROR', true);
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'project');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
